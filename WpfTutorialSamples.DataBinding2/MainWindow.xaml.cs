fead34a9620a74b4545497458f9e5dbd4e6dc583
Witek <Witek@DESKTOP-L5KJMIO>
2017-10-01 13:04:22 +0200

    przykład interakcji z przyciksami

---
+++ WpfTutorialSamples.DataBinding2/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.DataBinding2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //observable collection jest to typ listy który automatycznie się aktualizauje w widoku
+
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            users.Add(new User() { Name = "John Doe" });
+            users.Add(new User() { Name = "Jane Doe" });
+
+            lbUsers.ItemsSource = users;
+
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = "New user" });
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                (lbUsers.SelectedItem as User).Name = "Random Name";
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                users.Remove(lbUsers.SelectedItem as User);
+        }
+
+    }
+
+    public class User : INotifyPropertyChanged  //interfejs INotifyPropertyChanged służy do informowania widoku o zmianach we właściwościach klasy implementującej ten interfejs
+
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name;  }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+        
+        private void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
