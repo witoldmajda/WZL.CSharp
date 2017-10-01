fa6ca24d6bebf9bb3dfbfe4af8575e7312a00533
Witek <Witek@DESKTOP-L5KJMIO>
2017-10-01 12:02:10 +0200

    zmiany03

---
+++ WpfTutorialSamples.DataBinding/ChangeNotificationSample.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace WpfTutorialSamples.DataBinding
+{
+    public partial class ChangeNotificationSample : Window
+    {
+        private List<User> users = new List<User>();
+
+        public ChangeNotificationSample()
+        {
+            InitializeComponent();
+
+            users.Add(new User() { Name = "John Doe" });
+            users.Add(new User() { Name = "Jane Doe" });
+
+            lbUsers.ItemsSource = users;
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = "New user" });
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                (lbUsers.SelectedItem as User).Name = "Random Name";
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                users.Remove(lbUsers.SelectedItem as User);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
